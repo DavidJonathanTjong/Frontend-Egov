4782e84bf10b8581d3ca52c7a9bd31140efc31cc
Muhammad Henry Aditya <adityahenry2@gmail.com>
2025-04-20 12:36:54 +0800

    Refactor login page

--- src/apiService.ts
+++ src/apiService.ts
@@ -1,74 +1,83 @@
-import axios from "axios";
-import Router from "next/router";
-import toast from "react-hot-toast";
 import Cookies from "js-cookie";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { jwtDecode } from "jwt-decode";
+// import { createUserStore } from "../app/data/UserStore"; // Import the UserStore
+
+// const { setUser } = createUserStore(); // Create the user store instance
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BACKEND,
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers":
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
   },
 });
 
-let isRefreshing = false; // Apakah refresh token sedang diproses
-let refreshSubcribers: ((token: string) => void)[] = []; // Menyimpan permintaan yang menunggu token baru
+let isRefreshing = false;
+let refreshSubscribers: ((newToken: string) => void)[] = [];
 
-// // Fungsi untuk memproses ulang permintaan yang tertunda
-const onRefreshed = (newAccessToken: string) => {
-  refreshSubcribers.forEach((callback) => callback(newAccessToken));
-  refreshSubcribers = [];
+const onRefreshed = (newToken: string) => {
+  refreshSubscribers.forEach((callback) => callback(newToken));
+  refreshSubscribers = [];
 };
 
-// // interceptor permintaan
-api.interceptors.request.use((config) => {
+// Request interceptor
+api.interceptors.request.use(async (config) => {
   const token = Cookies.get("token");
   if (token) {
     config.headers["Authorization"] = `Bearer ${token}`;
+    // await fetchUserData(token);
   }
   return config;
 });
 
-// // interceptor response
+// Response interceptor
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
 
-    // Jika status 401, maka lakukan refresh token
     if (
       error.response &&
       error.response.status === 401 &&
       !originalRequest._retry
     ) {
-      originalRequest._retry = true; // mencegah chain request
+      originalRequest._retry = true;
 
       if (!isRefreshing) {
         isRefreshing = true;
 
         try {
-          const refreshToken = await api.post("/auth/refresh", {
-            refresh_token: Cookies.get("refresh_token"),
+          const refreshToken = Cookies.get("refresh_token");
+          const response = await api.post("/auth/refresh", {
+            refresh_token: refreshToken,
           });
 
-          const newAccessToken = refreshToken.data.token;
-          Cookies.set("token", newAccessToken, { expires: 1 });
-          onRefreshed(newAccessToken);
+          const newToken = response.data.token;
+          Cookies.set("token", newToken);
+          onRefreshed(newToken);
           isRefreshing = false;
-        } catch (error) {
-          console.log("Refresh token invalid or expired ", error);
+        } catch (refreshError) {
+          console.error("Refresh token invalid or expired", refreshError);
           Cookies.remove("token");
           Cookies.remove("refresh_token");
-          toast.error("Sesi Anda telah berakhir. Silakan login kembali.");
-          Router.push("/login");
-          return Promise.reject(error);
+          toast.error("Your session has expired. Please log in again.", {
+            duration: 5000,
+            position: "top-center",
+          });
+          isRefreshing = false;
+          return Promise.reject(refreshError);
         }
       }
 
-      // Tambahkan ke antrean
-      return new Promise((res) => {
-        refreshSubcribers.push((newToken) => {
+      return new Promise((resolve) => {
+        refreshSubscribers.push((newToken) => {
           originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
-          res(api(originalRequest));
+          resolve(api(originalRequest));
         });
       });
     }
@@ -76,5 +85,16 @@ api.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+// Function to fetch user data
+// export const fetchUserData = async (token: string) => {
+//   try {
+//     const response = await api.get(`/users/list/${jwtDecode(token).sub}`); // Adjust the endpoint as needed
+//     setUser(response.data); // Update the user store with the fetched data
+//   } catch (error) {
+//     console.error("Failed to fetch user data", error);
+//     throw error;
+//   }
+// };
 
 export default api;
