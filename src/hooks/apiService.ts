f52b98c498ceae5dc780da8243174dfed4786363
Muhammad Henry Aditya <adityahenry2@gmail.com>
2025-06-26 13:45:41 +0800

    Fix: Add Accept header

--- src/hooks/apiService.ts
+++ src/hooks/apiService.ts
@@ -9,7 +9,7 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BACKEND,
   headers: {
     "Content-Type": "application/json",
-    
+    "Accept": "application/json",
   },
 });
 
